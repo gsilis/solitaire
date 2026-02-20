0f6d26538c70332b7725a7bb38c8e452ac952606
Girts Silis <girts.silis@gmail.com>
2026-02-19 18:45:05 -0600

    + card graphic object

--- src/graphics/card-graphic.ts
+++ src/graphics/card-graphic.ts
@@ -1,9 +1,13 @@
 import { Actor, ActorArgs } from "excalibur";
 import { CardSide } from "../data/card-side";
 import { Resources } from "../resources";
+import { Card } from "card-shoe/cards/card";
+
+type CardGraphicOpts = ActorArgs & { card: Card }
 
 export class CardGraphic extends Actor {
   private _side: CardSide = CardSide.BACK
+  private _card: Card
 
   get side() { return this._side }
   public flip() {
@@ -14,5 +18,10 @@ export class CardGraphic extends Actor {
       this._side = CardSide.BACK
       this.graphics.use(Resources.CardBack.toSprite())
     }
+  }
+
+  constructor({ card, ...restOpts }: CardGraphicOpts) {
+    super({ ...restOpts })
+    this._card = card
   }
 }
