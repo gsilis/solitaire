2b6ff5dd01d671a137d2258a53c74d462eec0ff5
Girts Silis <girts.silis@gmail.com>
2026-02-26 09:52:36 -0600

    + menu first

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ const game = new Engine({
   // fixedUpdateTimestep: 16 // Turn on fixed update timestep when consistent physic simulation is important
 });
 
-game.start('table', { // name of the start scene 'start'
+game.start('menu', { // name of the start scene 'start'
   loader, // Optional loader (but needed for loading images/sounds)
 }).then(() => {
   // Do something after the game starts
